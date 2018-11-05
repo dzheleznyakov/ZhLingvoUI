74a0f295122a5fe98671d9a223ee3878ddfd813f
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2018-11-05 15:59:23 +0000

    Fix the content top margin

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import styles from './Layout.module.css';
+
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
-    <React.Fragment>
-        <Toolbar />
-        This is my webpage
-    </React.Fragment>
+  <React.Fragment>
+    <Toolbar />
+    <div className={styles.Content}>
+      This is my webpage content
+    </div>
+  </React.Fragment>
 );
 
 export default layout;
