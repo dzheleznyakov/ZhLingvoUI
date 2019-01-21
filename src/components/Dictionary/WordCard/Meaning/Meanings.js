92c650f17e6e8a666cf204f9b2741438c78ca78c
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2019-01-21 16:42:38 +0000

    Enable adding new translations

--- src/components/Dictionary/WordCard/Meaning/Meanings.js
+++ src/components/Dictionary/WordCard/Meaning/Meanings.js
@@ -25,29 +25,42 @@ const meanings = (props) => {
       placeholder='##' />
    ) : null;
 
-  let meaning = null;
-  if (props.translations) {
-    const onTranslationEdited = (index) => (value) => props.editTranslation(props.branch, index, value);
-    const onElaborationEdited = (index) => (value) => props.editElaboration(props.branch, index, value);
+  const onTranslationEdited = (index) => (value) => props.editTranslation(props.branch, index, value);
+  const onElaborationEdited = (index) => (value) => props.editElaboration(props.branch, index, value);
+  let meaning = [];
+  if (props.translations) {    
+    const getElaboration = (elaboration, index) => (
+      elaboration ? (
+        <Elaboration edited={onElaborationEdited(index)}>{elaboration}</Elaboration> 
+      ) : props.editMode ? (
+        <PromptSpan
+          cssClasses={classes.ElaborationPlaceholder}
+          edited={onElaborationEdited(index)}
+          prefix=" ("
+          postfix=")" />
+      ) : null
+    );
 
     meaning = props.translations.reduce((arr, { translation, elaboration }, index, tArray) => {
       const length = tArray.length;
       const span = (<span key={`t${index}`}>
         <Translation edited={onTranslationEdited(index)}>{translation}</Translation>
-        {elaboration ? (
-          <Elaboration edited={onElaborationEdited(index)}>{elaboration}</Elaboration> 
-        ) : props.editMode ? (
-          <PromptSpan
-            cssClasses={classes.ElaborationPlaceholder}
-            edited={onElaborationEdited(index)}
-            prefix=" ("
-            postfix=")" />
-        ) : null}
+        {getElaboration(elaboration, index)}
         {index < length - 1 ? '; ' : ''}
       </span>);
       arr.push(span);
       return arr;
     }, (remark ? [remark] : []));
+  }
+
+  if (props.editMode) {
+    const index = props.translations.length;
+    meaning.push((
+      <span key='prompt-translation'>
+        {'; '}
+        <PromptSpan edited={onTranslationEdited(index)} />
+      </span>
+    ));
   }
   
   return <span>{meaning}</span>;
