edc17d095a20197e01784e0eb7be315554ebc411
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2019-09-16 18:43:16 +0100

    Word Card Control: use hooks

--- src/components/Dictionary/WordCard/WordCardControl/WordCardControl.js
+++ src/components/Dictionary/WordCard/WordCardControl/WordCardControl.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import classes from './WordCardControl.module.scss';
 
@@ -8,12 +9,8 @@ import ControlPanel from '../../../UI/ControlPanel/ControlPanel';
 import * as actions from '../../../../store/actions/';
 
 const wordCardControl = (props) => {
-  const onExpansionToggled = () => {
-    props.setEditMode(false);
-  };
-  const onEditToggled = () => {
-      props.setEditMode(!props.editMode);
-  };
+  const onExpansionToggled = () => props.setEditMode(false);
+  const onEditToggled = () => props.setEditMode(!props.editMode);
   const entries = [{
     label: 'Edit',
     element: <ToggleButton checked={props.editMode} toggled={onEditToggled} />,
@@ -35,6 +32,11 @@ const mapDispatchToProps = dispatch => {
   return {
     setEditMode: (editMode) => dispatch(actions.setEditMode(editMode)),
   };
+};
+
+wordCardControl.propTypes = {
+  setEditMode: PropTypes.func.isRequired,
+  editMode: PropTypes.bool.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(wordCardControl);
